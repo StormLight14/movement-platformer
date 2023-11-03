47e178a3cc906a295aa20b7bc6ddbbe4a5ad7aa7
Storm <stormpower44@protonmail.com>
2023-11-02 20:57:41 -0600

    Update player.rs

--- src/player.rs
+++ src/player.rs
@@ -16,7 +16,10 @@ impl Plugin for PlayerPlugin {
 }
 
 #[derive(Component)]
-pub struct Player;
+pub struct Player {
+    pub jumps: u32,
+    pub jumps_left: u32,
+}
 
 fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
     let player_texture = asset_server.load("player.png");
@@ -35,27 +38,49 @@ fn spawn_player(mut commands: Commands, asset_server: Res<AssetServer>) {
             },
             ..default()
         },
-        Player,
+        Player {
+            jumps: 2,
+            jumps_left: 2,
+        },
         Name::from("Player"),
         Velocity::zero(),
         RigidBody::Dynamic,
         LockedAxes::ROTATION_LOCKED,
+        KinematicCharacterController::default(),
         Friction::new(0.1),
         Collider::cuboid(8.0, 8.0),
     ));
 }
 
 fn player_movement(
-    mut query: Query<(&mut Velocity, &Friction, &mut Transform), With<Player>>,
+    mut query: Query<
+        (
+            &mut Player,
+            &mut Velocity,
+            &Friction,
+            &mut Transform,
+            &KinematicCharacterControllerOutput,
+        ),
+        With<Player>,
+    >,
     input: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
-    for (mut vel, friction, mut transform) in query.iter_mut() {
+    for (mut player, mut vel, friction, mut transform, controller_output) in query.iter_mut() {
         let left_input = input.any_pressed([KeyCode::A, KeyCode::Left]);
         let right_input = input.any_pressed([KeyCode::D, KeyCode::Right]);
 
-        if input.just_pressed(KeyCode::Space) {
-            vel.linvel.y = JUMP_STRENGTH;
+        info!(player.jumps_left);
+
+        if controller_output.grounded {
+            player.jumps_left = player.jumps;
+        }
+
+        if player.jumps_left > 0 {
+            if input.just_pressed(KeyCode::Space) {
+                vel.linvel.y = JUMP_STRENGTH;
+                player.jumps_left -= 1;
+            }
         }
 
         let x_axis = -(left_input as i8) + right_input as i8;
